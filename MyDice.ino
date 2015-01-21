c9dc7a7e303679e0ff45072a989dd82b6e53b395
hopo28 <hopo28@gmail.com>
2015-01-21 11:00:30 +0000

    added piezo buzzer and additional countdown times
    mute function also added

--- MyDice.ino
+++ MyDice.ino
@@ -4,18 +4,24 @@
 #include <stdint.h>
 #include <Entropy.h>
 
-#define BUT_ROLL_PIN 3 //D3
+#define BUT_ROLL_PIN 3 //D2
 #define BUT_MODE_PIN A0
+#define PIEZO_PIN 2//needs to be a pwm one i think
 #define AUC_MIN 10
 #define AUC_MAX 45
 #define PRESS_SHORT 30//in milliseconds, for debouncing
 #define ROLL_DELAY 100
 
+#define DEBUG 1//serial debug
+
 boolean rollPressFlag = false;
 unsigned long rollPressedTimer = 0;
 boolean modePressFlag = false;
 unsigned long modePressedTimer = 0;
+boolean mute = false;
+char timerLength = 0; // 0 means random between auc limits
 
+//countdown animation
 unsigned char animation[][2] = {{0,2},{0,3},{0,4},{0,5},{1,6},{2,7},{3,7},{4,7},{5,7},{6,6},{7,5},{7,4},{7,3},{7,2},{6,1},{5,0},{4,0},{3,0},{2,0},{1,1}};
 
 byte standbyCounter = 0;
@@ -23,9 +29,9 @@ byte standbyCounter = 0;
 extern unsigned char diceFont[][8];
 byte Brightness = 31;//starts dim
 
-// modes are: Standby,1D6,2D6,Timer,Brightness
+// modes are: Standby,1D6,2D6,Timer Set,Timer,Brightness,Mute
 char mode = 0;//curent mode
-const char numModes = 5;
+const char numModes = 7;
 
 void setup()
 {
@@ -33,15 +39,19 @@ void setup()
   Entropy.initialize();
   pinMode(BUT_ROLL_PIN, INPUT_PULLUP);
   pinMode(BUT_MODE_PIN, INPUT_PULLUP);
+  pinMode(PIEZO_PIN, OUTPUT);
   doDisplay();
-//  Serial.begin(9600);
+  if (DEBUG)
+    Serial.begin(9600);
 }
+
 
 void loop()
 { 
   checkRoll();
   checkMode();
 }
+
 
 void checkMode()
 {
@@ -58,7 +68,7 @@ void checkMode()
     }
     else
     {
-      mode++;
+      mode++;//advance mode counter
       if(mode >= numModes)
       {
         mode = 0;
@@ -77,14 +87,15 @@ void checkMode()
           drawMyChar(6,0,0,0xFFFFFF);
           drawMyChar(1,0,0,0xFFFFFF);
           break;    
-        default: doDisplay(); break;//standby, timer, and brightness end up here
+        default: doDisplay(); break;//modes other than rolls end up here
       }
     }
   }
 }
 
+
 void checkRoll()
-{
+{//actually do the roll
   if(digitalRead(BUT_ROLL_PIN) == LOW)//is being pressed
   {
     rollPressFlag = true;
@@ -100,9 +111,15 @@ void checkRoll()
     {
       switch(mode)
       {
-        case 0: standbyCounter++; if(standbyCounter>=3){standbyCounter=0;} doDisplay(); break;
-        case 3: doTimer(); break; // Timer
-        case 4: // Brightness
+        case 0: standbyCounter++; if(standbyCounter>=3){standbyCounter=0;} doDisplay(); break;//standby
+        case 3:// Set timer
+            timerLength = timerLength + 15;//advance by 15 seconds
+            if(timerLength > 60)
+              timerLength = 0;
+            doDisplay();
+            break; 
+        case 4: doTimer(); break; // Timer
+        case 5: // Brightness
             Brightness = Brightness + 32;//will rollover
             doDisplay();
             for(int i = 0; i < ((Brightness+1)/32); i++)
@@ -110,6 +127,7 @@ void checkRoll()
               Rb.setPixelXY(0, i, reduceBrightness(0x00FF00, Brightness));
             }
             break;
+        case 6: mute = !mute; doDisplay(); break; // Mute
         default: doDisplay(); break; // Standby, 1D6 and 2D6 end up here
       }
       rollPressFlag = false;
@@ -158,8 +176,24 @@ void doDisplay()
         drawMyChar(dice1,0,0,dice1Col);//2D6(1)
         drawMyChar(dice2 + 6,0,0,dice2Col);//2D6(2)
         break;
-    case 3: drawMyChar(19,0,0,0xFFFFFF); break; // Timer
-    case 4: drawMyChar(20,0,0,0xFFFFFF); break; // Brightness
+    case 3:// Timer Set
+        switch(timerLength)
+        {
+          case 0: drawMyChar(28,0,0,0xFFFFFF); break;//draw Random
+          case 15: drawMyChar(24,0,0,0xFFFFFF); break;//draw 15
+          case 30: drawMyChar(25,0,0,0xFFFFFF); break;//draw 30
+          case 45: drawMyChar(26,0,0,0xFFFFFF); break;//draw 45
+          case 60: drawMyChar(27,0,0,0xFFFFFF); break;//draw 60
+          default: timerLength = 0; drawMyChar(28,0,0,0xFFFFFF); break;//draw Random
+        }
+        break;
+    case 4: drawMyChar(19,0,0,0xFFFFFF); break; // Timer
+    case 5: drawMyChar(20,0,0,0xFFFFFF); break; // Brightness
+    case 6:// Mute
+        drawMyChar(22,0,0,0xFFFFFF);//draw speaker
+        if(mute)//if muted
+        drawMyChar(23,0,0,0xFF0000);//draw slash
+        break;
     default: /*do nothing*/ break;
   }
 }
@@ -167,7 +201,9 @@ void doDisplay()
 void doTimer()
 {//perform the countdown
   unsigned long startTime = millis();//get time now
-  unsigned long time = Entropy.random(AUC_MIN,AUC_MAX);//get time length
+  unsigned long time = timerLength;
+  if(time == 0)
+    time = Entropy.random(AUC_MIN,AUC_MAX);//get random time length
   unsigned long endTime = startTime + (time*1000);//calculate end time
   Rb.blankDisplay();
   drawMyChar(19,0,0,0x00FF00);//in green
@@ -176,31 +212,40 @@ void doTimer()
   {//do animation
     drawMyChar(19,0,0,0x00FF00);//in green
     Rb.setPixelXY(animation[pixel][0], animation[pixel][1], reduceBrightness(0xFF0000, Brightness));//black out a pixel
-    delay(50);
+    delay(40);//10ms for display etc
     pixel++;
     if(pixel >= 20)
       pixel = 0;
+    if(time != 0)
+    {//if not random time, beep for last 3 seconds
+      if((endTime-millis() > 3000) && (endTime-millis() < 3050))
+        shortBip();
+      if((endTime-millis() > 2000) && (endTime-millis() < 2050))
+        shortBip();
+      if((endTime-millis() > 1000) && (endTime-millis() < 1050))
+        shortBip();
+    }
   }
   byte tempBrightness = Brightness;//save brightness
   Brightness = 255;//full brightness
   //Flash it a bit
+  for(int i = 0; i < 3; i++)
+  {
+    drawMyChar(19,0,0,0xFF0000);//in red
+    bip();//make a noise
+//    delay(200);//pause for a bit
+    Rb.blankDisplay();
+    delay(100);//pause for a bit
+  }
+  //long on
   drawMyChar(19,0,0,0xFF0000);//in red
-  delay(200);//pause for a bit
-  Rb.blankDisplay();
-  delay(200);//pause for a bit
-  drawMyChar(19,0,0,0xFF0000);//in red
-  delay(200);//pause for a bit
-  Rb.blankDisplay();
-  delay(200);//pause for a bit
-  drawMyChar(19,0,0,0xFF0000);//in red
-  delay(200);//pause for a bit
-  Rb.blankDisplay();
-  delay(200);//pause for a bit
-  drawMyChar(19,0,0,0xFF0000);//in red
-  delay(2000);//pause for a bit
+  timeEndBeep();//1 second sound
+  delay(1000);//pause for a bit longer
   Brightness = tempBrightness;//restore brightness
   doDisplay();
 }
+
+// = = = = = = = = = SUPPORT FUNCTIONS = = = = = = = = = = =
 
 uint32_t getColourFromNumber(char number)
 {//based on monopoly property colours
@@ -247,3 +292,5 @@ void drawMyChar(unsigned char ascii,unsigned int poX, unsigned int poY,  uint32_
     }
   }
 }
+
+
